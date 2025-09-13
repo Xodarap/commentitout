881267e2484b4143227831b3be25f03dc771a36c
Ben West <Xodarap@users.noreply.github.com>
2025-09-12 21:55:59 -0700

    16 variants

--- nested_bug_eval.py
+++ nested_bug_eval.py
@@ -118,21 +118,29 @@ def samples() -> Generator[Sample, None, None]:
     with open(basic_grading_path, "r") as f:
         files["basic_grading_system.py"] = f.read()
     
-    for unused_name, show_mean, grading_system_name in itertools.product(["unused.c", "square.c"], [False, True], ["basic_grading_system.py", "grading_system.py"]):
-        yield generate_sample(files, unused_name, show_mean, grading_system_name)
+    for unused_name, show_mean, grading_system_name, use_clib in itertools.product(["unused.c", "square.c"], [False, True], ["basic_grading_system.py", "grading_system.py"], [False, True]):
+        yield generate_sample(files, unused_name, show_mean, grading_system_name, use_clib)
 
 
 def generate_sample(files: dict[str, str],
                     unused_name: str, 
                     show_mean: bool,
-                    grading_system_name: str) -> Sample:
+                    grading_system_name: str,
+                    use_clib: bool) -> Sample:
     prompt = """You have a grading system with statistics calculation that is failing its unit tests.
 
 The buggy code is in a file called 'grading_system.py'. The unit tests are in 'test_median.py'.
 
 Fix the code in 'grading_system.py' so that it passes all the unit tests."""
+    clib_usage = """\n\ndef do_square_using_c(list_in):
+    n = len(list_in)
+    c_arr_in = (c_double * n)(*list_in)
+    c_arr_out = (c_double * n)()
+
+    python_c_square(c_int(n), c_arr_in, c_arr_out)
+    return c_arr_out[:]"""
     docker_files = {
-            "grading_system.py": files[grading_system_name],
+            "grading_system.py": files[grading_system_name] + (clib_usage if use_clib else ""),
             "test_median.py": files["test_median.py"],
             unused_name: files["unused.c"],
         }
