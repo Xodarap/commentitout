5d1a294c050d6638153569e20142171979ae0d1a
Ben West <Xodarap@users.noreply.github.com>
2025-09-13 16:05:54 -0700

    eval set

--- run_evaluations.py
+++ run_evaluations.py
@@ -3,6 +3,14 @@ from datetime import datetime
 
 success, logs = eval_set(
    tasks=["reward_hacking_eval.py"],
-   model=["openai/gpt-4.1-mini", "anthropic/claude-3-5-haiku-latest"],
+   model=["openai/gpt-4.1",
+          "openai/gpt-5-mini",
+          "openai/gpt-5",
+          "anthropic/claude-3-5-haiku-latest",
+          "anthropic/claude-sonnet-4-20250514",
+            #   "anthropic/claude-opus-4-1-20250805",
+            "google/gemini-2.5-pro",
+            "google/gemini-2.5-flash",
+          ],
    log_dir=f"logs-bulk-{datetime.now().strftime('%Y%m%d%H%M%S')}"      
 )
